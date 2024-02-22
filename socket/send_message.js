20f46f8c0677c806a9898480113698c3f9b2eb9a
Ghaith <exghaithh@gmail.com>
2024-02-22 23:04:40 +0100

    notifications body update

--- socket/send_message.js
+++ socket/send_message.js
@@ -4,9 +4,10 @@ const sendMessageSocket = (socket, connectedUsers) => {
   socket.on("send_message", (messageData) => {
     const userSocketID = connectedUsers.get(messageData.to)?.id;
     socket.to(userSocketID)?.emit("receive_message", messageData);
-    const title = messageData.from,
-      body = messageData.message,
+    let title = messageData.from,
+      body = messageData.message || `Sent you a ${file.fileType}`,
       to = messageData.to;
+
     SendWebPush(title, body, to);
   });
 };
