76905a6372397c6cf1764a8f62e046b4667797f5
Ghaith <exghaithh@gmail.com>
2024-02-23 15:36:31 +0100

    GetPostLikedUsers fix

--- routes/post/userList/get_post_liked_users.js
+++ routes/post/userList/get_post_liked_users.js
@@ -3,20 +3,23 @@ const pool = require("../../../pg_pool");
 require("dotenv").config();
 
 const GetPostLikedUsers = async (req, res) => {
-  const { postID } = req.query;
+  const { postID, username } = req.query;
   const client = await pool.connect().catch((err) => console.log(err));
 
   try {
-    if (!postID) {
+    if (!(postID && username)) {
       res.status(400).json("data missing");
       return;
     }
 
     const userListQuery = await client.query(
       `SELECT DISTINCT username, first_name,last_name, admin, verified 
-      FROM user_tbl, post_like_tbl 
-      WHERE username=liked_user AND liked_post=$1`,
-      [postID]
+FROM user_tbl
+JOIN post_like_tbl ON username=liked_user AND liked_post=$1
+LEFT JOIN follow_tbl f2 ON u.username = f2.following AND f2.follower = $1
+LEFT JOIN follow_request_tbl fr ON u.username = fr.req_following AND fr.req_follower = $1
+`,
+      [postID, username]
     );
 
     let userList = [];
