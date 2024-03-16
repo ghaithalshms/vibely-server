b8e5195f69dbf9f4dbf2deab21673cce37ff65bf
Ghaith <exghaithh@gmail.com>
2024-03-16 14:14:02 +0100

    max file size updated : 30mb

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const { postLink, getLink, deleteLink, updateLink } = require("./API_LINK");
 const app = express();
 app.use(cors());
 
-app.use(bodyParser.json({ limit: "3mb" }));
-app.use(bodyParser.urlencoded({ limit: "3mb", extended: true }));
-app.use(express.json({ limit: "3mb" }));
-app.use(express.urlencoded({ extended: true, limit: "3mb" }));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 
 const server = http.createServer(app);
 
