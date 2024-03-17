e39224a4c3ed634dd344c49ec512b5e7e66cc940
Ghaith <exghaithh@gmail.com>
2024-03-17 11:31:09 +0100

    pool error handle

--- pg_pool.js
+++ pg_pool.js
@@ -5,4 +5,9 @@ let pool = new Pool({
   max: 20,
 });
 
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
 module.exports = pool;
