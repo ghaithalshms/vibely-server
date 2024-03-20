5c42ac54a7ef3c3185e9da027dd45ab40b90b094
Ghaith <exghaithh@gmail.com>
2024-03-20 16:24:19 +0100

    error handling added

--- socket/send_message.js
+++ socket/send_message.js
@@ -2,17 +2,21 @@ const SendWebPush = require("../web_push_notification/send_web_push");
 
 const sendMessageSocket = (socket, connectedUsers) => {
   socket.on("send_message", (messageData) => {
-    const userSocketID = connectedUsers.get(messageData.to)?.id;
-    socket.to(userSocketID)?.emit("receive_message", messageData);
-    let title = messageData.from,
-      body = messageData.fileType.startsWith("text")
-        ? messageData.message
-        : `Sent you ${
-            messageData.fileType.split("/").charAt(0) === "v" ? "a" : "an"
-          }` + messageData.fileType.split("/")[0];
-    to = messageData.to;
+    try {
+      const userSocketID = connectedUsers.get(messageData.to)?.id;
+      socket.to(userSocketID)?.emit("receive_message", messageData);
+      let title = messageData.from,
+        body = messageData.fileType.startsWith("text")
+          ? messageData.message
+          : `Sent you ${
+              messageData.fileType.split("/").charAt(0) === "v" ? "a" : "an"
+            }` + messageData.fileType.split("/")[0];
+      to = messageData.to;
 
-    if (!userSocketID) SendWebPush(title, body, to);
+      if (!userSocketID) SendWebPush(title, body, to);
+    } catch (err) {
+      console.log(err);
+    }
   });
 };
 module.exports = sendMessageSocket;
