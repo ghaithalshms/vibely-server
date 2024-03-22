80b07c173d99d03ca61d9b4d882f40f9f49db676
Ghaith <exghaithh@gmail.com>
2024-03-22 22:00:44 +0100

    code optimized

--- routes/auth/sign_in.js
+++ routes/auth/sign_in.js
@@ -3,61 +3,63 @@ const jwt = require("jsonwebtoken");
 const pool = require("../../pg_pool");
 
 const signIn = async (req, res) => {
-  const { usernameOrEmail, password } = req.body;
-  const client = await pool.connect().catch((err) => console.log(err));
-
+  const client = await pool.connect();
   try {
-    if (!(usernameOrEmail && password)) {
-      res.status(400).json("data missing");
-      return;
+    const { usernameOrEmail, password } = req.body;
+    if (!usernameOrEmail || !password) {
+      return res
+        .status(400)
+        .json({ error: "Username/email or password is missing" });
     }
 
-    const usernameOrEmailVerified = usernameOrEmail.toLowerCase().trim();
+    const user = await getUserByUsernameOrEmail(client, usernameOrEmail);
 
-    const generateUniqueBrowserID = () => {
-      return Math.random().toString(16).substring(2) + Date.now().toString(16);
-    };
-
-    if (usernameOrEmailVerified && password) {
-      const tokenResult = await client.query(
-        `SELECT username, password, token_version FROM user_tbl 
-        WHERE username = $1 OR email = $1`,
-        [usernameOrEmailVerified]
-      );
-
-      if (tokenResult.rows.length > 0) {
-        if (tokenResult.rows[0].password === password) {
-          const token = jwt.sign(
-            {
-              username: tokenResult.rows[0].username,
-              tokenVersion: tokenResult.rows[0].token_version,
-            },
-            process.env.JWT_SECRET_KEY,
-            {
-              expiresIn: "1000d",
-            }
-          );
-
-          const username = tokenResult.rows[0].username;
-          const browserID = generateUniqueBrowserID();
-
-          res.status(200).json({ token, username, browserID });
-        } else {
-          if (!res.headersSent)
-            res.status(401).json("Password is not correct!");
-        }
-      } else {
-        if (!res.headersSent) res.status(404).json("User not exist!");
-      }
-    } else {
-      if (!res.headersSent) res.status(404).json(`Empty input`);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
-  } catch (err) {
-    console.log("unexpected error : ", err);
-    res.status(500).json(err);
+
+    if (user.password !== password) {
+      return res.status(401).json({ error: "Incorrect password" });
+    }
+
+    const token = generateToken(user);
+    const browserID = generateUniqueBrowserID();
+
+    res.status(200).json({ token, username: user.username, browserID });
+  } catch (error) {
+    console.error("Unexpected error:", error);
+    res.status(500).json({ error: "Internal server error" });
   } finally {
-    client?.release();
+    if (client) {
+      client.release();
+    }
   }
+};
+
+const getUserByUsernameOrEmail = async (client, usernameOrEmail) => {
+  const usernameOrEmailVerified = usernameOrEmail.toLowerCase().trim();
+  const query = `SELECT username, password, token_version FROM user_tbl 
+                 WHERE username = $1 OR email = $1`;
+
+  const result = await client.query(query, [usernameOrEmailVerified]);
+  return result.rows[0];
+};
+
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      username: user.username,
+      tokenVersion: user.token_version,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "1000d",
+    }
+  );
+};
+
+const generateUniqueBrowserID = () => {
+  return Math.random().toString(16).substring(2) + Date.now().toString(16);
 };
 
 module.exports = signIn;
