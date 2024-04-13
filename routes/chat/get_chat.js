0dc4f253385cf721c19fa8f230c205f1fbd919f2
Ghaith <exghaithh@gmail.com>
2024-04-13 11:28:56 +0200

    get older chat messages

--- routes/chat/get_chat.js
+++ routes/chat/get_chat.js
@@ -2,15 +2,26 @@ const checkToken = require("../../func/check_token");
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const getChatMessages = async (client, tokenUsername, otherUsername) => {
+const getChatMessages = async (
+  client,
+  tokenUsername,
+  otherUsername,
+  oldestMessageGot
+) => {
+  const messageIDInstructionString =
+    oldestMessageGot > 0 ? "AND msg_id < $3" : "AND msg_id > $3";
+
   return await client.query(
     `SELECT msg_id, message, msg_from, msg_to, sent_date, seen, file_type FROM message_tbl
-    WHERE (msg_to = $1 AND msg_from = $2) 
-    OR (msg_to = $2 AND msg_from = $1)
+    WHERE (
+      (msg_to = $1 AND msg_from = $2) 
+      OR (msg_to = $2 AND msg_from = $1)
+    )
+    ${messageIDInstructionString}
     ORDER BY msg_id DESC
-    LIMIT 50
+    LIMIT 20
   `,
-    [tokenUsername, otherUsername]
+    [tokenUsername, otherUsername, oldestMessageGot]
   );
 };
 
@@ -31,7 +42,7 @@ const formatChatMessages = (chatQuery) => {
 };
 
 const GetChat = async (req, res) => {
-  const { token, username } = req.query;
+  const { token, username, oldestMessageGot } = req.query;
   const pool = new Pool({ connectionString: process.env.DATABASE_STRING });
   const client = await pool.connect();
   client.on("error", (err) =>
@@ -49,10 +60,16 @@ const GetChat = async (req, res) => {
       return;
     }
 
-    const chatQuery = await getChatMessages(client, tokenUsername, username);
-    const chatList = formatChatMessages(chatQuery);
+    const chatQuery = await getChatMessages(
+      client,
+      tokenUsername,
+      username,
+      oldestMessageGot
+    );
+    const chatArray = formatChatMessages(chatQuery);
 
-    if (!res.headersSent) res.send(chatList);
+    if (!res.headersSent)
+      res.send({ chatArray: chatArray, oldestMessageGot: chatArray[0].id });
   } catch (err) {
     console.log("unexpected error : ", err);
     res.status(500).json(err);
