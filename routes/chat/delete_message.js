9d175a48adc97eca57d7965f2297c6b632c730c5
Ghaith <exghaithh@gmail.com>
2024-04-15 12:47:24 +0200

    delete message fixed

--- routes/chat/delete_message.js
+++ routes/chat/delete_message.js
@@ -4,17 +4,20 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const DeleteMessageFromDB = async (req, res) => {
-  const { token, messageID } = req.body;
   const pool = new Pool({ connectionString: process.env.DATABASE_STRING });
-  const client = await pool.connect();
+  const client = await pool.connect().catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   client.on("error", (err) =>
     console.error("something bad has happened!", err.stack)
   );
 
   try {
+    const { token, messageID } = req.body;
     if (!(token && messageID))
       if (!res.headersSent) {
-        return res.status(400).json("missing data");
+        return res.status(400).json(req);
       }
     const tokenUsername = await CheckTokenNoDB(token);
     if (tokenUsername === false) {
@@ -24,10 +27,11 @@ const DeleteMessageFromDB = async (req, res) => {
       async (isMsgSentByUser) => {
         if (isMsgSentByUser) {
           await deleteMessageFromDB(client, messageID);
-          if (!res.headersSent) res.status(200).json(result);
+          if (!res.headersSent)
+            res.status(200).json("Message deleted successfully");
         } else {
           if (!res.headersSent)
-            res.status(400).json("This message sas not sent by you!");
+            res.status(400).json("This message was not sent by you!");
         }
       }
     );
@@ -41,12 +45,12 @@ const DeleteMessageFromDB = async (req, res) => {
 
 module.exports = DeleteMessageFromDB;
 
-async function verifyMessageSentByUser(client, tokenUsername, messageID) {
+async function verifyMessageSentByUser(client, messageID, tokenUsername) {
   const result = await client.query(
     "SELECT msg_id, msg_from FROM message_tbl WHERE msg_id = $1 AND msg_from = $2",
     [messageID, tokenUsername]
   );
-  return result.rows.count > 0;
+  return result.rows.length > 0;
 }
 
 async function deleteMessageFromDB(client, messageID) {
